663f88393d00890aeabac7a46c4b342541f1632b
George <327052186@qq.com>
2020-07-06 15:55:16 +0800

    优化mysql批量插入

--- src/main/java/com/kams/common/DBConn.java
+++ src/main/java/com/kams/common/DBConn.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConn {
-	private static  Connection connection;
-	
+	private static Connection connection;
+
 	/*
 	 * public static Connection getConn() { try {
 	 * Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); String
@@ -17,15 +17,16 @@ public class DBConn {
 	public static Connection getConn() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url="jdbc:mysql://localhost:3306/george?serverTimezone=UTC&allowMultiQueries=true";
-			String username="root";
-			String password="f10011116";
-			connection=DriverManager.getConnection(url,username,password);
+			String url = "jdbc:mysql://localhost:3306/george?serverTimezone=UTC&allowMultiQueries=true";
+			String username = "root";
+			String password = "f10011116";
+			connection = DriverManager.getConnection(url, username, password);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return connection;
 	}
+
 	public static void main(String[] args) {
 		System.out.println(getConn());
 	}
